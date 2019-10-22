c85c6fd35d0f54fd3d57ba5f6be888bb4cdb8367
huasheng li <huashengli@v1040-wn-rt-c-71-129.campus-dynamic.uwaterloo.ca>
2019-10-22 17:34:12 -0400

    Binding

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { Student } from '../../shared/Student';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css'],
+})
+
+export class ServerComponent {
+  ServerID = 10;
+  serverStatus = 'online';
+  counter = 0;
+  userName: string;
+  isLogin = false;
+
+  getServerStatus () {
+    return this.serverStatus;
+  }
+  countplus () {
+    return this.counter++;
+  }
+
+  reset () {
+    return this.counter = 0;
+  }
+
+  onUpdateinput (event: Event) {
+    this.userName = (<HTMLInputElement>event.target).value;
+    console.log(this.userName);
+  }
+
+  login () {
+    this.isLogin = true;
+  }
+
+  logout () {
+    this.isLogin = false;
+  }
+}
+
